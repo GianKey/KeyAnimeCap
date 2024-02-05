23fbc72868993c88dd4a12d7125c98e49711b4b0
GianKey <625087947@qq.com>
2024-02-05 22:34:27 +0800

    调整动画驱动代码

--- mocaprender/mmpose/mmposeApi.js
+++ mocaprender/mmpose/mmposeApi.js
@@ -22,11 +22,36 @@ exports.start_mmpose_inference = async function(inputPath) {
         },
       });
   
-      console.log('上传成功', response.data); // 注意这里使用response.data来获取响应内容
+      console.log('推理成功', response.data); // 注意这里使用response.data来获取响应内容
       return response.data; // 返回响应数据
     } catch (error) {
-      console.error('上传失败', error);
+      console.error('推理失败', error);
       throw error; // 抛出错误以供调用方处理
     }
   };
   
+
+  
+// exports.voideUpload = async function(inputPath) {
+//   try {
+//     const videoFormData = new FormData();
+//     videoFormData.append('fileName', inputPath.name);
+//     const videoStream = fs.createReadStream(inputPath.path);
+//     videoFormData.append('videoFile', videoStream);
+
+//     const path = '/videoprocessing/video_upload/';
+//     const url = default_protocol + default_hostname + ':' + default_port + path;
+
+//     const response = await axios.post(url, videoFormData, {
+//       headers: {
+//         ...videoFormData.getHeaders(), // 获取FormData的headers
+//       },
+//     });
+
+//     console.log('上传成功', response.data); // 注意这里使用response.data来获取响应内容
+//     return response.data; // 返回响应数据
+//   } catch (error) {
+//     console.error('上传失败', error);
+//     throw error; // 抛出错误以供调用方处理
+//   }
+// };
